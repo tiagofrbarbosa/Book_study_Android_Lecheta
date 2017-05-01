3727ee674962725f7106d1960675a66c3ab4090a
Tiago Barbosa <tfreirebar@gmail.com>
2017-05-01 00:57:01 -0300

    Debug Android

---
+++ Cap04_debugActivity/app/src/main/java/tech/infofun/cap04_debugactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package tech.infofun.cap04_debugactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends DebugActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
